8bb032cff2b551cbe69eb8f84cdd0eb1df58e0a0
Stuti Sharma <stuti.sharma@accolitedigital.com>
2022-11-22 09:33:34 +0530

    Java Object to JSON

---
+++ Plant.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Plant{
+    private String name;
+    private int leaves;
+    private int flowers;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLeaves(int leaves) {
+        this.leaves = leaves;
+    }
+    public void setFlowers(int flowers) {
+        this.flowers = flowers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLeaves() {
+        return leaves;
+    }
+
+    public int getFlowers() {
+        return flowers;
+    }
+
+}
